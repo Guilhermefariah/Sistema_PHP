a519f28ced0889a497ad27f91caa5fcf868be0ec
Guilhermefariah <guilhermefaria321@gmail.com>
2025-02-14 13:43:54 -0300

    Composer

---
+++ lib/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'login/php',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f7734b1f7d21cf59f665b97287cce71fb5040003',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'login/php' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f7734b1f7d21cf59f665b97287cce71fb5040003',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
